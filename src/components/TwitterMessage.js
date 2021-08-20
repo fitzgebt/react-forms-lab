f70fe869f7b8046a596e97816993007a2b06ff2d
fitzgebt <fitzgebt@gmail.com>
2021-08-20 15:57:15 -0700

    commit tree 0eabc6614ef1d08db457c8ff23699e5679d94be6

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      counter: 280,
+    };
+  }
+
+  handleChange = (event) => {
+    const newCount = 280 - event.target.value.length
+    this.setState({
+      message: event.target.value,
+      counter: newCount,
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+        <p>{this.state.counter} chars remaining</p>
       </div>
     );
   }
